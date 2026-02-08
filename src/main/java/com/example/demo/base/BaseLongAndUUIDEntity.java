74ed85da758f957fa808b31bf1eb1a285b144ba0
Ilkhomjon <maxamadqulovilkhomjon@gmail.com>
2026-02-08 23:59:54 +0500

    Base LongAndUUIDEntity, UserEntity, DotEnvConfig classes added

---
+++ src/main/java/com/example/demo/base/BaseLongAndUUIDEntity.java
@@ -0,0 +1,39 @@
+package com.example.demo.base;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseLongAndUUIDEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false, nullable = false)
+    private Long id;
+
+    @Column(name = "uuid", nullable = false, updatable = false, unique = true)
+    private UUID uuid;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        if (uuid == null) {
+            uuid = UUID.randomUUID();
+        }
+    }
+}
